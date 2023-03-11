ff3ffbb005deec3b1a4722f75c1be0b1e495eaab
Martín Campuzano <41243969+simowd@users.noreply.github.com>
2023-03-11 14:53:06 -0400

    Update install-work.sh

--- install-work.sh
+++ install-work.sh
@@ -2,12 +2,12 @@
 sudo npm install -g @angular/cli
 
 #Installing Rider
-wget -O Rider.tar.gz https://download.jetbrains.com/rider/JetBrains.Rider-2022.1.2.tar.gz
+wget -O Rider.tar.gz https://download.jetbrains.com/rider/JetBrains.Rider-2022.3.2.tar.gz
 sudo tar -xzf Rider.tar.gz -C /opt
 
 rm -f Rider.tar.gz
 
-cd /opt/'JetBrains Rider-2022.1.2'/bin
+cd /opt/'JetBrains Rider-2022.3.2'/bin
 sh ./rider.sh
 
 
